bd8e80af91433b1c655762f775741e3dccbc25e9
Joao Ramos <joaopramos076@gmail.com>
2026-08-17 18:15:18 -0300

    Fix: tsconfig Prettier and linter

--- src/app/__tests__/index-test.tsx
+++ src/app/__tests__/index-test.tsx
@@ -1,13 +1,13 @@
-import { render } from '@testing-library/react-native';
 import HomeScreen, { CustomText } from '@/app/index';
+import { render } from '@testing-library/react-native';
 
 describe('<HomeScreen/>', () => {
-    test('Text renders correctly on HomeScreen', async () => {
-        const { getByText } = await render (<HomeScreen />);
-    });
+  test('Text renders correctly on HomeScreen', async () => {
+    await render(<HomeScreen />);
+  });
 
-    test('CustomText renders correctly', async () => {
-        const tree = (await render(<CustomText>Some text</CustomText>)).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  test('CustomText renders correctly', async () => {
+    const tree = (await render(<CustomText>Some text</CustomText>)).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
