9aeca13d9197ce336bfdc97a92702e658d12112e
Joao Ramos <joaopramos076@gmail.com>
2026-08-19 19:42:16 -0300

    Fix: nativewind and metro config conflit

--- metro.config.ts
+++ metro.config.ts
@@ -1,5 +1,6 @@
 import { getDefaultConfig } from 'expo/metro-config';
+import { withNativeWind } from 'nativewind/metro';
 
 const config = getDefaultConfig(__dirname);
 
-export default config;
+export default withNativeWind(config, { input: './src/global.css' });
