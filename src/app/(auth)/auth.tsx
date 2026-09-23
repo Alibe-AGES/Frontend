0e1f529fe7071f9a75faf071328e12befda742c5
Joao Ramos <joaopramos076@gmail.com>
2026-09-23 17:04:54 -0300

    Fix: pr

--- src/app/(auth)/auth.tsx
+++ src/app/(auth)/auth.tsx
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export { AuthScreen as default } from '@/screens/AuthScreen';
-=======
 export { default } from '@/screens/Auth/controller';
->>>>>>> b9ce7953709fd0b9757db54d22f4a941e8162168
--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export { LoginScreen as default } from '@/screens/LoginScreen';
-=======
 export { default } from '@/screens/Login/controller';
->>>>>>> b9ce7953709fd0b9757db54d22f4a941e8162168
